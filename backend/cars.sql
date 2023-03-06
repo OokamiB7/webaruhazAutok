472d673dc72cbbd8017bddc9f3ccfb92472c37d6
OokamiB7 <bencebence227@gmail.com>
2023-03-06 09:17:28 +0100

    Tesztadatok2

--- backend/cars.sql
+++ backend/cars.sql
@@ -10,5 +10,16 @@
   insert into product (id,name,price,quantity) VALUES
     (1,'Féktárcsa','3500','3');
 
+  insert into users (id,name,password) VALUES
+    (1,'Admin','adminpassword');
+
+    insert into cart (id,userid,productid,quantity) values
+      (10,1,1,2);
+
     select * from product;
+    select * from users;
+    select * from cart;
+
     delete from product;
+    delete from users;
+    delete from cart;
