35cc59d736cde67ced1ad2940ee7b9425369d63b
OokamiB7 <bencebence227@gmail.com>
2023-03-06 09:11:07 +0100

    Tesztadatok

--- backend/cars.sql
+++ backend/cars.sql
@@ -3,3 +3,12 @@
   CREATE DATABASE carparts
 	CHARACTER SET utf8
 	COLLATE utf8_hungarian_ci;
+
+
+  # 2. Tesztadatok
+
+  insert into product (id,name,price,quantity) VALUES
+    (1,'Féktárcsa','3500','3');
+
+    select * from product;
+    delete from product;
