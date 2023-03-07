ac0a82d7d5aadd0b91d6db3aed2f1d2bc3ae7f7e
OokamiB7 <bencebence227@gmail.com>
2023-03-07 10:30:22 +0100

    BackendMegvan

--- backend/cars.sql
+++ backend/cars.sql
@@ -5,13 +5,54 @@
 	COLLATE utf8_hungarian_ci;
 
 
-  # 2. Tesztadatok
+  # 2. táblák
 
-  insert into product (id,name,price,quantity) VALUES
+    CREATE TABLE carparts.users (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  firstName VARCHAR(50) DEFAULT NULL,
+  lastName VARCHAR(255) DEFAULT NULL,
+  gender VARCHAR(255) DEFAULT NULL,
+  userName VARCHAR(255) DEFAULT NULL,
+  email VARCHAR(50) DEFAULT NULL,
+  password VARCHAR(255) DEFAULT NULL,
+  number INT(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8,
+COLLATE utf8_hungarian_ci;
+
+
+    CREATE TABLE carparts.product (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  productName VARCHAR(255) DEFAULT NULL,
+  price INT(11) DEFAULT NULL,
+  quantity INT(11) DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8,
+COLLATE utf8_hungarian_ci;
+
+
+  CREATE TABLE carparts.cart (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  userid INT(11) NOT NULL,
+  productid INT(11) NOT NULL,
+  quantity INT(11) NOT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8,
+COLLATE utf8_hungarian_ci;
+
+  # 3. Tesztadatok
+
+  insert into product (id,productName,price,quantity) VALUES
     (1,'Féktárcsa','3500','3');
 
-  insert into users (id,name,password) VALUES
-    (1,'Admin','adminpassword');
+  insert into users (id,firstName,lastName,gender,userName,email,password,number) VALUES
+    (1,'Kiss','Ádám',1,'bonedragonHUN666','kiss.adam@gmail.com',123456,062042069);
 
     insert into cart (id,userid,productid,quantity) values
       (10,1,1,2);
