7a2c0dfbb26cfeda9a783ce103238263f8381389
OokamiB7 <bencebence227@gmail.com>
2023-04-14 11:15:03 +0200

    modal

--- frontend/src/views/AruhazView.vue
+++ frontend/src/views/AruhazView.vue
@@ -13,12 +13,15 @@
               
               <div class="card-body">
                 <h5 class="card-title" v-html="product.productName"></h5>
+                <p> Ár: {{ product.price}}Ft</p>
+                <p> Raktáron: {{ product.quantity}}DB</p>
+
                 <button
                   type="button"
                   class="btn btn-primary"
                   data-bs-toggle="modal"
                   data-bs-target="#exampleModal"
-                 
+                 @click="onClickReszletek(hal.FejezetId)"
                 >
                   Kosárba
                 </button>
@@ -28,6 +31,61 @@
         </div>
       </div>
     
+    <!-- modal -->
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel" v-html="productKartya.productName">
+             
+            </h1>
+            
+          </div>
+          <div class="modal-body">
+            <table class="table table-success">
+              <thead>
+                <tr>
+                  <th>Ár: </th>
+                  <th>Darab</th>
+                  <th>Raktáron</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{{ productKartya.price }}</td>
+                  <td>{{ productKartya.quantity }}</td>
+                  <td>{{ productKartya.isInStock }}</td>
+                </tr>
+              </tbody>
+            </table>
+
+            <!-- <img
+                :src="`/public/kepek/${halKartya.KepFile}`"
+                class="card-img-top"
+                alt="..."
+              /> -->
+
+             
+
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
       
   </div>
 </template>
@@ -47,29 +105,39 @@ export default {
     return {
       products: [],
       urlProducts: "http://localhost:3000/products",
-      productKartya: new ProductKartya()
+      productKartya: new ProductKartya(),
+      productId: null
     };
     
   },
   mounted(){
-    this.getProductKartya();
+    this.getProductKartyak();
   },
   methods: {
-    async getProductKartya() {
+    async getProductKartyak() {
       const response = await fetch(this.urlProducts);
       const data = await response.json();
       this.products = data.data;
     },
-    async getHalkartya() {
+    async getProductKartya() {
       const urlProducts = `${this.urlProducts}`;
       const response = await fetch(urlProducts);
       const data = await response.json();
       this.productKartya = data.data[0];
     },
+        onClickReszletek(productId) {
+      this.productId = productId;
+      this.getProductKartya();
+    },
+
   }
 }
 </script>
 
 <style>
+.modal-body {
+  max-height: calc(100vh - 180px);
+  overflow-y: auto;
+}
   
 </style>
